a6cc84dff2eb6c249e15257c6cbd1950f7f9d6a8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-05-25 15:57:38 +0530

    Update main.tf

--- Infrastructure/VirtualNetwork/main.tf
+++ Infrastructure/VirtualNetwork/main.tf
@@ -14,23 +14,35 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {}
 
 #Create Resource Group
-resource "azurerm_resource_group" "rg-dnlh-12mar-dev" {
+resource "azurerm_resource_group" "example" {
   name     = "rg-dnlh-12mar-dev"
   location = "eastus"
 }
 
-#Create Virtual Network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-dnlh-12mar-dev"
-  address_space       = ["192.168.0.0/16"]
-  location            = "eastus"
-  resource_group_name = azurerm_resource_group.rg-dnlh-12mar-dev.name
+resource "azurerm_network_security_group" "example" {
+  name                = "example-security-group"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
-# Create Subnet
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet"
-  resource_group_name  = azurerm_resource_group.rg-dnlh-12mar-dev.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefix       = "192.168.0.0/24"
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.example.id
+  }
+
+  tags = {
+    environment = "Production"
+  }
 }
