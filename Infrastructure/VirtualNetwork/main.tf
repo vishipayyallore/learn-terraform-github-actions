51771d25a8234efa0c444bcc61e2d1af199c8fe1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-05-25 15:43:55 +0530

    Initial Version

---
+++ Infrastructure/VirtualNetwork/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-dnlh-12mar-dev"
+    storage_account_name = "sttfstatedhls"
+    container_name       = "terraformstate"
+    key                  = "terraformstate.tfstate"
+  }
+}
+
+provider "azurerm" {
+  # The "feature" block is required for AzureRM provider 2.x.
+  # If you're using version 1.x, the "features" block is not allowed.
+  version = "~>2.0"
+  features {}
+}
+
+data "azurerm_client_config" "current" {}
+
+#Create Resource Group
+resource "azurerm_resource_group" "rg-dnlh-12mar-dev" {
+  name     = "rg-dnlh-12mar-dev"
+  location = "eastus"
+}
+
+#Create Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-dnlh-12mar-dev"
+  address_space       = ["192.168.0.0/16"]
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.rg-dnlh-12mar-dev.name
+}
+
+# Create Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.rg-dnlh-12mar-dev.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "192.168.0.0/24"
+}
+  
