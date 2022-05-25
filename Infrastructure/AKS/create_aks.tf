93a3cff54259c2068a1696db18580f00ab75ca92
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-05-25 16:19:40 +0530

    AKS First Cut

---
+++ Infrastructure/AKS/create_aks.tf
@@ -0,0 +1,43 @@
+data "azurerm_resource_group" "k8s" {
+  name = "rg-dnlh-12mar-test"
+}
+
+resource "azurerm_kubernetes_cluster" "k8s" {
+  name                = var.cluster_name
+  location            = data.azurerm_resource_group.k8s.location
+  resource_group_name = data.azurerm_resource_group.k8s.name
+  dns_prefix          = var.dns_prefix
+
+  linux_profile {
+    admin_username = "ubuntu"
+
+    ssh_key {
+      key_data = file(var.ssh_public_key)
+    }
+  }
+
+  default_node_pool {
+    name       = "agentpool"
+    node_count = var.agent_count
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  # addon_profile {
+  key_vault_secrets_provider {
+    secret_rotation_enabled = true
+  }
+  # }
+
+  network_profile {
+    load_balancer_sku = "standard"
+    network_plugin    = "kubenet"
+  }
+
+  tags = {
+    Environment = "Development"
+  }
+}
---
+++ Infrastructure/AKS/main.tf
@@ -0,0 +1,24 @@
+terraform {
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.2.0"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+
+    backend "azurerm" {
+    resource_group_name  = "rg-dnlh-12mar-dev"
+    storage_account_name = "sttfstatedhls"
+    container_name       = "terraformstate"
+    key                  = "terraformstateaks.tfstate"
+  }
+
+}
+
+
+provider "azurerm" {
+  features {}
+}
